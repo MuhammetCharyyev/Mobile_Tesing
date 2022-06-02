1c3697313691b3f236ec1404bc5b8b33052c1064
Muhammet <muhammedabuchary@gmail.com>
2022-06-02 12:31:44 -0500

    Initial commit

---
+++ src/test/java/com/cydeo/tests/CalculatorOperationsTest.java
@@ -0,0 +1,103 @@
+package com.cydeo.tests;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.*;//common assertion package
+
+public class CalculatorOperationsTest {
+
+    AppiumDriver<MobileElement> driver;
+
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        // caps.setCapability("deviceName", "Pixel 3");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 3");
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        caps.setCapability(MobileCapabilityType.APP, "https://cybertek-appium.s3.amazonaws.com/calculator.apk" );
+
+        //set URL for the appium server
+        URL url = new URL("http://localhost:4723/wd/hub");
+
+        //launch appiumDriver
+        driver = new AndroidDriver<MobileElement>(url, caps);
+    }
+
+    /**
+     * 4 + 5 = 9
+     */
+
+    @Test
+    public void addTest() {
+        //locate 4 ,plus, 5, equals, and result
+        //location determined by standard xpath
+        MobileElement four = driver.findElement(By.id("com.google.android.calculator:id/digit_4"));
+        four.click();
+
+        MobileElement plus = driver.findElement(MobileBy.AccessibilityId("plus"));
+        plus.click();
+
+        MobileElement five = driver.findElement(By.id("com.google.android.calculator:id/digit_5"));
+        five.click();
+
+        //here we used 'MobileBy' instead of 'By' as Appium Inspector shows
+        // 'AccessibilityId' instead of regular 'id'
+        MobileElement equals = driver.findElement(MobileBy.AccessibilityId("equals"));
+        equals.click();
+
+        MobileElement result = driver.findElement(MobileBy.id("com.google.android.calculator:id/result_final"));
+
+        String actResult = result.getText();
+        System.out.println("actResult = " + actResult);
+
+        int expResult = 9;
+
+        assertEquals(expResult, Integer.parseInt(actResult));
+        // 'Integer.parseInt' helps to transform above-mentioned "9" from String to int
+    }
+
+    /**
+     * 15 / 5 = 3
+     */
+    @Test
+    public void divideTest() {
+        MobileElement one = driver.findElement(By.id("com.google.android.calculator:id/digit_1"));
+        one.click();
+
+        MobileElement five = driver.findElement(By.id("com.google.android.calculator:id/digit_5"));
+        five.click();
+
+        MobileElement divide = driver.findElement(MobileBy.AccessibilityId("divide"));
+        divide.click();
+
+        five.click();
+
+        MobileElement equals = driver.findElement(MobileBy.AccessibilityId("equals"));
+        equals.click();
+
+        MobileElement result = driver.findElement(MobileBy.id("com.google.android.calculator:id/result_final"));
+
+        String actResult = result.getText();
+        System.out.println("actResult = " + actResult);
+
+        assertEquals(3 , Integer.parseInt(actResult));
+
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        driver.closeApp();
+    }
+
+}
